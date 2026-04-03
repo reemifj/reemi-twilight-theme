6b973dd51bd175b32acd2af3375b549b93321058
reemifj <reemi.fj@gmail.com>
2026-04-03 10:54:50 +0300

    Remove Twilight Watcher Plugin integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Import Twilight Watcher Plugin if available
-let TwilightWatcherPlugin;
-try {
-  TwilightWatcherPlugin = require('@anthropic-ai/twilight-watcher-plugin');
-} catch (e) {
-  // Twilight Watcher not installed, skip
-}
-
-const plugins = [
-  new MiniCssExtractPlugin({
-    filename: 'app.css',
-  }),
-];
-
-if (TwilightWatcherPlugin) {
-  plugins.push(new TwilightWatcherPlugin());
-}
-
 module.exports = {
   entry: './src/assets/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
   },
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
@@ -42,7 +25,28 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      }
     ],
   },
-  plugins,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.scss', '.css'],
+  },
 };
